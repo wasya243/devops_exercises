971bbbb580525b85e9064f458589aa0d006ec017
Vasyl <vasyl@voicett.com>
2024-01-03 21:44:59 +0200

    add basic req logging.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
+
 const app = express()
 const port = 3000
+
+app.use((req, res, next) => {
+  console.log(`Request id: ${uuid()}, method: ${req.method}, url: ${req.url}`);
+
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
